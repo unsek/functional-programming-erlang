d308186b4079258b30d16dda614c73fa2f12579e
Sek Un <michael@boreddev.com>
2020-05-12 22:51:32 +0800

    Add sorted only implementation

---
+++ fourth.erl
@@ -0,0 +1,18 @@
+-module(fourth).
+-export([how_many_equal/3]).
+
+how_many_equal(A, A, A) ->
+  3;
+
+how_many_equal(A, B, C) ->
+  [X, Y, Z] = lists:sort([A, B, C]),
+  sorted_how_many_equal(X, Y, Z).
+
+sorted_how_many_equal(X, X, _) ->
+  2;
+
+sorted_how_many_equal(_, X, X) ->
+  2;
+
+sorted_how_many_equal(_, _, _) ->
+  0.
