5d285cb3e1c899f06b0b60f8636cdd6d94a3008d
Sek Un <michael@boreddev.com>
2020-05-12 19:18:52 +0800

    Remove imports for fully qualified names. Update comment to use @doc

--- first.erl
+++ first.erl
@@ -1,23 +1,23 @@
 -module(first).
 -export([double/1, mult/2, area/3, square/1, treble/1]).
 
-% Multiplies X and Y
+%% @doc Multiplies X and Y
 mult(X,Y) ->
   X*Y.
 
-% Doubles the amount of X
+%% @doc Doubles the amount of X
 double(X) ->
   mult(2, X).
 
-% Squares X
+%% @doc Squares X
 square(X) ->
   mult(X, X).
 
-% Triples the amount of X
+%% @doc Triples the amount of X
 treble(X) ->
   mult(3, X).
 
-% Finds the area
+%% @doc Finds the area
 area(A, B, C) ->
   S = (A + B + C)/2,
   math:sqrt(S*(S-A)*(S-B)*(S-C)).
--- second.erl
+++ second.erl
@@ -1,13 +1,12 @@
 -module(second).
--import('first', [square/1]).
 -export([hypotenuse/2, perimeter/2]).
 
-% Finds the hypotenuse of a right angled triangle given 2 sides
+%% @doc Finds the hypotenuse of a right angled triangle given 2 sides
 hypotenuse(A, B) ->
-  X = square(A) + square(B),
+  X = first:square(A) + first:square(B),
   math:sqrt(X).
 
-% Finds the perimeter of a right angled triangle given 2 short sides
+%% @doc Finds the perimeter of a right angled triangle given 2 short sides
 perimeter(A, B) ->
   X = hypotenuse(A, B),
   A + B + X.
